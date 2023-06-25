51119e1a917b6675bdeea9cdaa95b789cf8dd7e2
Manh Nguyen <manhnb1911@gmail.com>
2023-06-25 15:19:27 +0700

    .

--- UniT/Packages/UniT/Scripts/UI/IPresenterFactory.cs
+++ UniT/Packages/UniT/Scripts/UI/IPresenterFactory.cs
@@ -5,5 +5,18 @@ namespace UniT.UI
 
     public interface IPresenterFactory : IFactory<IPresenter, Type>
     {
+        public static class Factory
+        {
+            private class PresenterFactory : IPresenterFactory
+            {
+                private readonly Func<Type, IPresenter> factory;
+
+                public PresenterFactory(Func<Type, IPresenter> factory) => this.factory = factory;
+
+                public IPresenter Create(Type type) => this.factory(type);
+            }
+
+            public static IPresenterFactory Create(Func<Type, IPresenter> factory) => new PresenterFactory(factory);
+        }
     }
 }
