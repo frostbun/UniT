f7539c92d3bb288f4042c04c415a1214c1abe179
Manh Nguyen <manhnb1911@gmail.com>
2023-07-05 16:50:59 +0700

    async initializable

--- UniT/Packages/UniT/Scripts/Utilities/IInitializable.cs
+++ UniT/Packages/UniT/Scripts/Utilities/IInitializable.cs
@@ -1,7 +1,14 @@
 namespace UniT.Utilities
 {
+    using Cysharp.Threading.Tasks;
+
     public interface IInitializable
     {
         public void Initialize();
+    }
+
+    public interface IAsyncInitializable
+    {
+        public UniTask InitializeAsync();
     }
 }
