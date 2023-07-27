2861b825b87718710dbb9e8d430bb6bc5eb52d21
Manh Nguyen <manhnb1911@gmail.com>
2023-07-27 23:18:06 +0700

    add UniT.DependencyInjection.Slim

---
+++ UniT/Packages/UniT/Scripts/DependencyInjection/Slim/ServiceProvider.cs
@@ -0,0 +1,32 @@
+namespace UniT.DependencyInjection.Slim
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using UniT.Extensions;
+
+    public static class ServiceProvider
+    {
+        private static readonly Dictionary<Type, List<object>> Cache = new();
+
+        public static void Add<T>(T instance)
+        {
+            GetCache<T>().Add(instance);
+        }
+
+        public static T Get<T>()
+        {
+            return GetCache<T>().Cast<T>().Single();
+        }
+
+        public static T[] GetAll<T>()
+        {
+            return GetCache<T>().Cast<T>().ToArray();
+        }
+
+        private static List<object> GetCache<T>()
+        {
+            return Cache.GetOrAdd(typeof(T), () => new());
+        }
+    }
+}
