1072ce8ca280732f284cafa28a9de42fc7019d5d
Manh Nguyen <manhnb1911@gmail.com>
2023-11-03 17:02:58 +0700

    change CsvData to readonly

--- Scripts/Data/Csv/CsvData.cs
+++ Scripts/Data/Csv/CsvData.cs
@@ -1,27 +1,59 @@
 namespace UniT.Data.Csv
 {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using UniT.Extensions;
+    using UnityEngine.Scripting;
 
-    public class CsvData : ICsvData
+    public abstract class CsvData : ICsvData
     {
         Type ICsvData.RowType => this.GetType();
 
         void ICsvData.Add(object key, object value) => value.CopyTo(this);
     }
 
-    public class CsvData<T> : List<T>, ICsvData
+    [Preserve]
+    public class CsvData<T> : ICsvData, IReadOnlyList<T>
     {
         Type ICsvData.RowType => typeof(T);
 
-        void ICsvData.Add(object key, object value) => this.Add((T)value);
+        void ICsvData.Add(object key, object value) => this.list.Add((T)value);
+
+        IEnumerator IEnumerable.GetEnumerator() => this.list.GetEnumerator();
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() => this.list.GetEnumerator();
+
+        private readonly List<T> list = new();
+
+        public int Count => this.list.Count;
+
+        public T this[int index] => this.list[index];
     }
 
-    public class CsvData<TKey, TValue> : Dictionary<TKey, TValue>, ICsvData
+    [Preserve]
+    public class CsvData<TKey, TValue> : ICsvData, IReadOnlyDictionary<TKey, TValue>
     {
         Type ICsvData.RowType => typeof(TValue);
 
-        void ICsvData.Add(object key, object value) => this.Add((TKey)key, (TValue)value);
+        void ICsvData.Add(object key, object value) => this.dictionary.Add((TKey)key, (TValue)value);
+
+        IEnumerator IEnumerable.GetEnumerator() => this.dictionary.GetEnumerator();
+
+        IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator() => this.dictionary.GetEnumerator();
+
+        private readonly Dictionary<TKey, TValue> dictionary = new();
+
+        public int Count => this.dictionary.Count;
+
+        public TValue this[TKey key] => this.dictionary[key];
+
+        public IEnumerable<TKey> Keys => this.dictionary.Keys;
+
+        public IEnumerable<TValue> Values => this.dictionary.Values;
+
+        public bool ContainsKey(TKey key) => this.dictionary.ContainsKey(key);
+
+        public bool TryGetValue(TKey key, out TValue value) => this.dictionary.TryGetValue(key, out value);
     }
 }
